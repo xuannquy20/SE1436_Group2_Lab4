dbe9f719010cda320f51979e92719d523ea709c9
hoang1362000 <hoangvietle1331@gmail.com>
2021-07-05 10:30:37 +0700

    reportgui

--- SE1436_Group2_Lab4/GUI/ReportGUI.aspx.cs
+++ SE1436_Group2_Lab4/GUI/ReportGUI.aspx.cs
@@ -16,7 +16,17 @@ namespace SE1436_Group2_Lab4.GUI
 
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
-
+                GridView2.DataSource = null;
+                GridView2.DataBind();
+                numberRecord2.Visible = false;
+                SelectedDatesCollection sdc;
+                sdc = Calendar1.SelectedDates;
+                txtFrom.Text = sdc[0].ToString("yyyy-MM-dd");
+                txtTo.Text = sdc[sdc.Count - 1].ToString("yyyy-MM-dd");
+                DataTable data = ReportDAO.GetOrderByInformation(txtFrom.Text, txtTo.Text, TextBox3.Text, TextBox4.Text);
+                GridView1.DataSource = data;
+                GridView1.DataBind();
+                numberRecord1.Text = data.Rows.Count.ToString();
         }
     }
 }
